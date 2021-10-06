fedcf6ba3de4bad4d0b6391ce7701fc173ac2438
Paulo Rodrigues <paulo.rodrigues@rebase.com.br>
2021-10-06 20:53:28 -0300

    reorganized directories

--- config/environment/test.rb
+++ config/environment/test.rb
@@ -1,3 +1,7 @@
 ENV['RACK_ENV'] = 'test'
 
-Dir.glob('./src/**/*.rb').each { |f| require f }
+directories = %i[application domain infra]
+
+directories.each do |dir|
+  Dir.glob("./#{dir}/**/*.rb").each { |f| require f }
+end
